3d3cc85bd89444168621b3c57d85918c2723d8e5
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-23 20:58:13 +0530

    feat(order): implement create order and business rule for order service

--- backend/src/services/OrderService.ts
+++ backend/src/services/OrderService.ts
@@ -1,11 +1,73 @@
 import { CreateOrderDto } from "dtos/order/CreateOrderDto";
 import { BaseService } from "./BaseService";
-import { Order } from "generated/prisma";
+import { MenuItem, Order } from "generated/prisma";
 import { IOrderRepository } from "repositories/interfaces/IOrderRepository";
 
 class OrderService extends BaseService<CreateOrderDto, Order> {
     constructor(orderRepository: IOrderRepository) {
         super(orderRepository);
+    }
+
+    // we need to write the business logic.
+    // the current business logic is that
+    // when ordering a main dish is required
+    // there has to be one or more side dishes
+    // dessert is optional
+    // we implement a helper function to do this check
+
+    private async checkOrderLogic(orderDto: CreateOrderDto): Promise<void> {
+        // we can fetch the menu items with the getMenuItemsWithTypes method
+
+        // had to change it to any because even though we have included
+        // 'type' in the query, its still a relation name. So we have to use any type here
+        const menuItems: any[] = await this.repository.getMenuItemsWithTypes();
+
+        // the question doesnt state if two main dishes can be added in the order or not
+        // since it wasnt mentioned, ill go with the simplest way
+        // where one order has only 1 main dish
+
+        let mainDishCount = 0;
+        let sideDishCount = 0;
+        let dessertCount = 0;
+
+        for(const item of orderDto.items) {
+            // find which menu item it is
+            const menuItem = menuItems.find(menuItem => (menuItem.id === item.menuId));
+
+            // error handler to be implemented :)
+
+            if(!menuItem) {
+                throw new Error("Menu item not found!");
+            }
+
+            if (menuItem.type.name === "Main Dish") {
+                mainDishCount += item.quantity;
+            } else if (menuItem.type.name === "Side Dish") {
+                sideDishCount += item.quantity;
+            } else if (menuItem.type.name === "Dessert") {
+                dessertCount += item.quantity;
+            }
+
+            // we need 1 main dish
+            if(mainDishCount != 1) {
+                throw new Error("Need one main dish");
+            }
+
+            // we need at least 1 side dish
+            if(sideDishCount < 1) {
+                throw new Error("need at least one side dish");
+            }
+
+
+
+        }
+    }
+
+
+    public async create(dto: CreateOrderDto): Promise<Order> {
+        // first we need to check the business logic
+        await this.checkOrderLogic(dto);
+        return this.repository.create(dto);
     }
 
 }
