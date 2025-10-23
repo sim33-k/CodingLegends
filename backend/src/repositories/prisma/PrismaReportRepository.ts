5a4364fe7b071aef7bb0baace889445829e3ec6d
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-24 01:34:11 +0530

    feat(backend): implement getDailySalesRevenue prisma repo method

--- backend/src/repositories/prisma/PrismaReportRepository.ts
+++ backend/src/repositories/prisma/PrismaReportRepository.ts
@@ -1,5 +1,5 @@
 import { IReportRepository } from "repositories/interfaces/IReportRepository";
-
+import { database } from "lib/Database";
 
 // Daily sales revenue
 // Most famous main dish
@@ -10,7 +10,25 @@ import { IReportRepository } from "repositories/interfaces/IReportRepository";
 export class PrismaReportRepository implements IReportRepository {
 
     async getDailySalesRevenue(date: String): Promise<any> {
+        // SELECT SUM(Order.total) FROM "Order" WHERE DATE(Order.createdAt) = DATE($1)
+        // problem is that prisma doesnt have date function
+        // isntead we have to use greater than and less than
 
+        const startDate = new Date(`${date}T00:00:00.000Z`);
+        const endDate = new Date(`${date}T23:59:59.999Z`);
+
+        const result = await database.order.aggregate({
+            _sum: {
+                total: true
+            },
+            where: {
+                date: {
+                    gte: startDate, lte: endDate
+                }
+            }
+        })
+
+        return result;
     }
 
     async getFamousMainDish(): Promise<any> {
