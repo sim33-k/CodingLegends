7257546d6d318c2a47ffc5b30b859caa239dd74b
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-23 18:37:17 +0530

    feat(order): Implement createOrder method in prisma

--- backend/src/repositories/prisma/PrismaOrderRepository.ts
+++ backend/src/repositories/prisma/PrismaOrderRepository.ts
@@ -5,5 +5,53 @@ import { CreateOrderDto, CreateOrderItemDto } from "dtos/order/CreateOrderDto";
 
 
 class PrismaOrderRepository implements IOrderRepository {
-    
+
+    public async createOrder(order: CreateOrderDto): Promise<Order> {
+        // since the total is stored in the order table, it is set to zero
+        let total = 0;
+        const orderItems = [];
+
+        // the order dto has the order items inside it
+        // since there are few menu items, we can fetch them all at the same time
+
+        const menuItems =  await database.menuItem.findMany({});
+        // ill add the error handlers later
+
+        for (const item of order.items) {
+            const menuItem = menuItems.find(menuItem => menuItem.id === item.menuId);
+            if (!menuItem) {
+                throw new Error("Menu item not found!");
+            }   
+            const price = item.quantity * Number(menuItem.price); // must convert to number because of type
+            total += price;
+
+            // now we can add the order items to the array
+            orderItems.push({
+                menuId: item.menuId,
+                quantity: item.quantity,
+                price: price
+            });
+        }
+
+        // now we can create an order with the items
+
+        const createdOrder = await database.order.create({
+            data: {
+                total: total,
+                OrderItem: {
+                    createMany: {
+                        data: orderItems
+                    }
+                }
+            },
+            include: {
+                OrderItem: true
+            }
+        })
+
+        return createdOrder;
+
+    }
 }
+
+export default PrismaOrderRepository
