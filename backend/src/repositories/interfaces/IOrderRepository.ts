245bddc1912a9c562f91eb8ea9efbbc015bb6a49
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-23 17:46:06 +0530

    feat(order): add IOrderRepository interface for order management

--- backend/src/repositories/interfaces/IOrderRepository.ts
+++ backend/src/repositories/interfaces/IOrderRepository.ts
@@ -1,6 +1,9 @@
 import { CreateOrderDto } from "dtos/order/CreateOrderDto";
 import { Order } from "generated/prisma";
 
+// i have added this repository pattern because I had an issue previously with prisma client + supabase
+// so if I face the same issue again, i can easily swap it out with supabase client
+
 interface IOrderRepository {
     createOrder(order: CreateOrderDto): Promise<Order>;
     getOrderById(orderId: number): Promise<Order | null>;
