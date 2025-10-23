08852b77881a4c2bfc225d360e0801a16ae0a9ca
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-23 16:46:40 +0530

    feat(order): implement CreateOrderDto and CreateOrderItemDto with validation

--- backend/src/dtos/order/createOrderDto.ts
+++ backend/src/dtos/order/createOrderDto.ts
@@ -1,8 +1,51 @@
-export interface createOrderItem {
-    menuItemId: number;
-    quantity: number;
+import Joi from "joi";
+
+
+// instead of creating dtos and validation seperately, i have combined it in classy way
+export class CreateOrderItemDto{
+    public menuId: number;
+    public quantity: number;
+
+    constructor(menuId: number,quantity: number) {
+        this.menuId = menuId;
+        this.quantity = quantity;
+    }
+
+    public static schema = Joi.object({
+        menuId: Joi.number().required(),
+        quantity: Joi.number().required()
+    });
+
+    public static validate(data: any) {
+        return this.schema.validate(data);
+    }
+
 }
 
-export interface createOrder {
-    items: createOrderItem[];
+
+// similarly, for create order
+export class CreateOrderDto{
+    public items: CreateOrderItemDto[];
+
+    constructor(items: CreateOrderItemDto[]) {
+        this.items = items;
+    }
+
+    public static schema = Joi.object({
+        items: Joi.array().items(CreateOrderItemDto.schema).required()
+    });
+
+    public static validate(data: any) {
+        return this.schema.validate(data);
+    }
+
 }
+
+// export interface createOrderItem {
+//     menuItemId: number;
+//     quantity: number;
+// }
+
+// export interface createOrder {
+//     items: createOrderItem[];
+// }
