688ef580f55c6ea80cfd33a15898190895d48b39
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-24 03:15:56 +0530

    feat(backend): add ReportService with all the methods in repo

---
+++ backend/src/services/ReportService.ts
@@ -0,0 +1,41 @@
+// I will not be extending the base service, because this service is specific to reports,
+
+import { IReportRepository } from "repositories/interfaces/IReportRepository";
+
+// Daily sales revenue
+// Most famous main dish
+// Most famous side dish
+// Which side dish is consumed most with which main dish
+// API endpoints (where applicable)
+// getDailySalesRevenue(date: String): Promise<any>;
+// getFamousMainDish(): Promise<any>;
+// getFamousSideDish(): Promise<any>;
+// getMostPopularSideDishForEachMainDish(): Promise<any>;
+
+
+
+// and will not be used for normal cruds
+export class ReportService {
+    private repository: IReportRepository;
+
+    constructor(reportRepository: IReportRepository) {
+        this.repository = reportRepository;
+
+    }
+
+    public async getDailySalesRevenue(date: String): Promise<any> {
+        return this.repository.getDailySalesRevenue(date);
+    }
+
+    public async getFamousMainDish(): Promise<any> {
+        return this.repository.getFamousMainDish();
+    }
+
+    public async getFamousSideDish(): Promise<any> {
+        return this.repository.getFamousSideDish();
+    }
+
+    public async getMostPopularSideDishForEachMainDish(): Promise<any> {
+        return this.repository.getMostPopularSideDishForEachMainDish();
+    }
+}
