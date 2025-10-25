8b4981ff2d9cbf34f2aa46241e4dfee3f95d02fa
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-25 19:17:13 +0530

    feat(frontend): add browser routes and pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import MenuOrder from './pages/MenuOrder'
+import Reports from './pages/Reports'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-900">
-        Hello world!
-      </h1>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MenuOrder />} />
+        <Route path="/reports" element={<Reports />} />
+      </Routes>
+    
+    </BrowserRouter>
+      
     </>
   )
 }
---
+++ frontend/src/pages/MenuOrder.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MenuOrder = () => {
+  return (
+    <div>
+      <h1>Menu Order</h1>
+    </div>
+  )
+}
+
+export default MenuOrder
