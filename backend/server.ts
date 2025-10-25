29f839a9a1e6984a7095ba776b2b8a9c4de96b42
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-26 02:11:39 +0530

    feat(backend): add cors support,

--- backend/server.ts
+++ backend/server.ts
@@ -2,8 +2,12 @@ import express from "express";
 import OrderRoutes from "./src/routes/OrderRoutes";
 import ReportRoutes from "./src/routes/ReportRoutes";
 import MenuRoutes from "./src/routes/MenuRoutes";
+import cors from "cors";
 
 const app = express();
+app.use(cors({ origin: "http://localhost:5173" }));
+
+
 app.use(express.json());
 
 // Removed the classy approach because it felt like an overkill
