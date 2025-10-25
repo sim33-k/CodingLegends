a6d017b9eeb1d8cd9f0df50095444841743d2ca6
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-26 00:16:50 +0530

    feat(frontend): removed multiple button instances

--- frontend/src/components/MenuPanel.tsx
+++ frontend/src/components/MenuPanel.tsx
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Input } from './ui/input'
 import { Button } from './ui/button'
+import { useEffect } from 'react'
 
 const MenuPanel = () => {
+
+  const [currentCategory, setCurrentCategory] = useState('all');
+
+  const types = [
+    {id: 'all', name: 'All Items'},
+    {id: 'main', name: 'Main Dishes'},
+    {id: 'side', name: 'Side Dishes'},
+    {id: 'dessert', name: 'Desserts'},
+  ]
+
+
   return (
     <div>
         <h2 className="text-2xl font-semibold border-b border-border pb-2 mb-4">
             Menu Items
         </h2>
       <div className='flex flex-wrap mb-1 gap-2'>
-        <Button variant="outline" className="mb-4">
+        {/* <Button variant="outline" className="mb-4">
           All Items
         </Button>
         <Button variant="outline" className="mb-4">
@@ -20,7 +32,10 @@ const MenuPanel = () => {
         </Button>
         <Button variant="outline" className="mb-4">
           Desserts
-        </Button>
+        </Button> */}
+        {types.map(type => 
+          <Button />
+        )}
       </div>
       <div>
         <Input placeholder="Search items" />
