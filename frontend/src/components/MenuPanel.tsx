692d835f4aee9a382e472bf6708a87ad5a8a0a67
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-26 00:04:06 +0530

    feat(frontend): add shadcn button component for menu item types

--- frontend/src/components/MenuPanel.tsx
+++ frontend/src/components/MenuPanel.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Input } from './ui/input'
+import { Button } from './ui/button'
 
 const MenuPanel = () => {
   return (
@@ -7,7 +8,24 @@ const MenuPanel = () => {
         <h2 className="text-2xl font-semibold border-b border-border pb-2 mb-4">
             Menu Items
         </h2>
-      <Input placeholder="search items" />
+      <div>
+        <Button variant="outline" className="mb-4">
+          All Items
+        </Button>
+        <Button variant="outline" className="mb-4">
+          Main Dishes
+        </Button>
+        <Button variant="outline" className="mb-4">
+          Side Dishes
+        </Button>
+        <Button variant="outline" className="mb-4">
+          Desserts
+        </Button>
+      </div>
+      <div>
+        <Input placeholder="Search items" />
+      </div>
+      
     </div>
   )
 }
