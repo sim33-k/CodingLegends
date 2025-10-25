61268a7f08ad4e80b5f7a0d87198debe336d2ff0
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-26 02:14:00 +0530

    fix(frontend): remove unused type prop from ItemCard component

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -16,12 +16,13 @@ type MenuItem = {
   type: MenuItem
 }
 
-const ItemCard = ({ item, onClick, type } : {item: MenuItem, onClick: any, type: MenuType}) => {
+const ItemCard = ({ item, onClick} : {item: MenuItem, onClick: any, type: MenuType}) => {
   return (
     <div onClick={onClick} key={item.id} className="border border-border p-4 bg-background  rounded-lg shadow-sm cursor-pointer
       transition-all duration-200 hover:shadow-md hover:scale-[1.02]">
       <h3 className="font-medium text-lg mb-1">{item.name}</h3>
       <p className=" text-sm mb-3">{item.price}</p>
+      <p className="text-xs text-muted-foreground">{item.type.name}</p>
       {/* <Button size="sm" className="w-full">
         Add to Order
       </Button> */}
