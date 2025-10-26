be5981047c04f5edf447a6283258d488fdd38028
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-27 02:12:41 +0530

    fix(frontend): fix typo in the card

--- frontend/src/components/section-cards.tsx
+++ frontend/src/components/section-cards.tsx
@@ -17,7 +17,7 @@ export function SectionCards({stats} :sectionCards ) {
     <div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
       <Card className="@container/card">
         <CardHeader>
-          <CardDescription>Daily Sales Revenue</CardDescription>
+          <CardDescription>Today's Sales Revenue</CardDescription>
           <CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
             Rs. {stats.dailySalesRevenue || "Loading"}
           </CardTitle>
