d604695f053bf66e6dff83d7ce28fc302233110e
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-26 18:01:45 +0530

    feat(frontend): add Summary component to OrderPanel for displaying order summary

---
+++ frontend/src/components/Order Panel/Summary.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Summary = () => {
+  return (
+    <div>
+      test
+    </div>
+  )
+}
+
+export default Summary
--- frontend/src/components/OrderPanel.tsx
+++ frontend/src/components/OrderPanel.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ItemCard from "./Order Panel/ItemCard";
 import type { OrderItem } from "@/types/Common";
 import ItemCardList from "./Order Panel/ItemCardList";
+import Summary from "./Order Panel/Summary";
 
 interface OrderPanelProps {
   orderItem: OrderItem[];
@@ -11,16 +12,17 @@ interface OrderPanelProps {
 
 const OrderPanel = ({orderItem, updateQuantity, removeFromOrder} : OrderPanelProps) => {
   return (
-    <div>
+    <div className="flex flex-col h-full">
       <h2 className="text-2xl font-semibold border-b border-border pb-2 mb-4">
         Current Order
       </h2>
 
-      <div>
-        {/* <ItemCard />
-        <ItemCard /> */}
-        {/* <ItemCardList orderItem={orderItem}/> */}
+      <div className="flex-1 overflow-y-auto space-y-3 mb-4">
         <ItemCardList items={orderItem} updateQuantity={updateQuantity} removeFromOrder={removeFromOrder} />
+      </div>
+
+      <div>
+        <Summary />
       </div>
     </div>
   );
