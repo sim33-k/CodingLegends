fa3209c1a1ddafb98de378f2b092b57016a1f92d
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-26 19:58:21 +0530

    fix(frontend): fix the bug where '=' was used instead of '==='

--- frontend/src/pages/MenuOrder.tsx
+++ frontend/src/pages/MenuOrder.tsx
@@ -19,11 +19,11 @@ const MenuOrder = () => {
     // check 1: we need to restrict 2 different main dishes to be added because the 
     // questions mentions 'a main dish', but the quantity can be many
 
-    const isMainDish = item.type.name = "Main Dish";
-    if(isMainDish) {
+    const isMainDish = item.type.name === "Main Dish";
+    if (isMainDish) {
       // we need to check if there are any other main dishes in the order
       const mainDishCheck = orderItem.find(x => x.type.name == "Main Dish" && x.id != item.id);
-      
+
       // if it exists, we need to issue and alert and not proceed further
       if (mainDishCheck) {
         setAlertState({show: true, type: 'error', title: 'Cannot add this main dish', message: 'Please order main dish of the same type!'});
@@ -39,20 +39,20 @@ const MenuOrder = () => {
 
       // showing the sucess alert
 
-      setAlertState({show: true, type: 'success', title: 'Added item successfully', message: `The item ${item.name} is added to the order!`});
+      setAlertState({ show: true, type: 'success', title: 'Added item successfully', message: `The item ${item.name} is added to the order!` });
 
       // it will be -1 if it doesnt exist
-      if(itemIndex > -1) {
+      if (itemIndex > -1) {
         const newItems = [...prev];
         newItems[itemIndex].quantity++;
-        return newItems; 
+        return newItems;
       } else {
         // if it doesnt exist, then its a brand new entry
 
-        return [...prev, {...item, quantity: 1}];
+        return [...prev, { ...item, quantity: 1 }];
       }
     })
-    
+
   }
 
   const updateQuantity = (id: number, value: number) => {
@@ -61,8 +61,8 @@ const MenuOrder = () => {
     } else {
       setOrderItem(prev => (
         prev.map(item => {
-          if(item.id === id) {
-            return {...item, quantity: value}
+          if (item.id === id) {
+            return { ...item, quantity: value }
           } else {
             return item;
           }
@@ -83,9 +83,9 @@ const MenuOrder = () => {
   // clean up logic for the alert
 
   useEffect(() => {
-    if(alertState.show) {
+    if (alertState.show) {
       const timer = setTimeout(() => {
-        setAlertState(prev => ({...prev, show: false}))
+        setAlertState(prev => ({ ...prev, show: false }))
       }, 2000);
       // clean up
       return () => clearTimeout(timer);
@@ -97,14 +97,14 @@ const MenuOrder = () => {
     <div>
       {/* alert logic */}
       <div>
-        {alertState.show ? <AlertComponent type={alertState.type} message={alertState.message} title={alertState.title}/> : null}
+        {alertState.show ? <AlertComponent type={alertState.type} message={alertState.message} title={alertState.title} /> : null}
       </div>
       <div className='flex flex-col lg:flex-row gap-6 min-h-[calc(100vh-8rem)]'>
         <div className='lg:w-2/3 border border-border p-4 bg-background  rounded-lg'>
-          <MenuPanel addToOrder={addToOrder}/>
+          <MenuPanel addToOrder={addToOrder} />
         </div>
         <div className='lg:w-1/3 border border-border p-4 bg-background  rounded-lg'>
-          <OrderPanel orderItem={orderItem} updateQuantity={updateQuantity} removeFromOrder={removeFromOrder} clearOrder={clearOrder} setAlertState={setAlertState}/>
+          <OrderPanel orderItem={orderItem} updateQuantity={updateQuantity} removeFromOrder={removeFromOrder} clearOrder={clearOrder} setAlertState={setAlertState} />
         </div>
       </div>
     </div>
