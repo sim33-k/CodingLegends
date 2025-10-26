7bc9466266a160fc18dbc088522bbe494ea9035b
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-27 04:20:58 +0530

    fix(backend): add getAllOrders method to orderController in OrderRoutes

--- backend/src/routes/OrderRoutes.ts
+++ backend/src/routes/OrderRoutes.ts
@@ -11,4 +11,5 @@ const orderController = new OrderController(orderService);
 // since i didnt add the arrow function in the method inside the controller,
 // the this reference is lost, so i will have to add the bind method every time
 router.post("/", orderController.createOrder.bind(orderController));
+router.get("/", orderController.getAllOrders.bind(orderController));
 export default router;
