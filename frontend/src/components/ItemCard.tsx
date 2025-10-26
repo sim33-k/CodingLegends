236e4f04ce240d128e97c295e0aa59c53a74cd38
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-26 07:39:05 +0530

    fix(frontend): Remove redundant key prop from ItemCard component

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -2,7 +2,7 @@ import type { MenuItem } from '@/types/Common'
 
 const ItemCard = ({ item, onClick} : {item: MenuItem, onClick: any}) => {
   return (
-    <div onClick={onClick} key={item.id} className="border border-border p-4 bg-background  rounded-lg shadow-sm cursor-pointer
+    <div onClick={onClick} className="border border-border p-4 bg-background  rounded-lg shadow-sm cursor-pointer
       transition-all duration-200 hover:shadow-md hover:scale-[1.02]">
       <h3 className="font-medium text-lg mb-1">{item.name}</h3>
       <p className=" text-sm mb-3">{item.price}</p>
