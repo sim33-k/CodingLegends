35b16218797ecd3d5997dc3acd388f043b78a25f
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-26 05:37:05 +0530

    feat(frontend): add item details and trash button

--- frontend/src/components/Order Panel/ItemCard.tsx
+++ frontend/src/components/Order Panel/ItemCard.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { Button } from '../ui/button'
+import { Trash2 } from 'lucide-react'
 
 const ItemCard = () => {
   return (
-    <div>
-      test2
+    <div className='border border-border rounded-lg p-3 bg-background'>
+      <div className='flex justify-between items-start mb-2'>
+        <div>
+            <h3 className="font-medium">Rice</h3>
+            <p className="text-sm text-muted-foreground">100</p>
+        </div>
+        <div>
+            <Button variant="ghost" size="sm" className="h-8 w-8 p-0 hover:bg-destructive/10 hover:text-destructive">
+                <Trash2 className="h-4 w-4" />
+            </Button>
+        </div>
+        
+      </div>
     </div>
   )
 }
