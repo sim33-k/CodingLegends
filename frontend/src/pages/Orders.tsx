9ef06c1f92c6e80cf15d1f414ca1536e47acae61
SW33TSTUFF <simaakniyaz@gmail.com>
2025-10-27 04:35:19 +0530

    feat(frontend): add Order and OrderItem types

--- frontend/src/pages/Orders.tsx
+++ frontend/src/pages/Orders.tsx
@@ -1,9 +1,37 @@
 import DataTablePagination from "@/components/DataTablePagination"
 
+// implementation of this data table is in the shadcn documentation
+
+type OrderItem = {
+  id: number
+  menuId: number
+  orderId: number
+  quantity:number
+  price: string
+  menu: {
+    id: number
+    name: string
+    price: number
+    typeId: number
+    type: {
+      id: number
+      name: string
+    }
+  }
+}
+
+type Order = {
+  id:number
+  date:string
+  total:string
+  OrderItem: OrderItem[]
+}
+
+
 const Orders = () => {
   return (
     <>
-      <DataTablePagination/>
+      <DataTablePagination columns={columns} data={orders}/>
     </>
   )
 }
